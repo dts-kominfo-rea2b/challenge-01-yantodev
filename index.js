d2ff4ba7e0a539a0ed97171f0cb63c152212424e
yanto <ekocahyanto007@gmail.com>
2022-05-26 21:17:35 +0700

    Challenge 5

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    name: 'Monica',
+    gender: 'Female',
+    age: 17,
+    email: 'monica@dingdong.com',
+    favoriteColor: ['Yellow', 'Pink', 'White', 'Purple'],
+    isHavePet: true,
+    education: [{
+            name: 'SD 01',
+            city: 'Jakarta',
+            graduate: 2016
+        },
+        {
+            name: 'SMP 02',
+            city: 'Jakarta',
+            graduate: 2019
+        },
+        {
+            name: 'SMA 03',
+            city: 'Tangerang',
+            graduate: null
+        },
+    ],
+    favoriteRestaurant: ['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+};
+const secondUser = {
+    name: 'Wendy',
+    gender: 'Male',
+    age: 23,
+    email: 'wendy@dingdong.com',
+    favoriteColor: ['Yellow', 'Pink', 'White', 'Purple'],
+    isHavePet: false,
+    education: [{
+            name: 'SD 02',
+            city: 'Jakarta',
+            graduate: 2010
+        },
+        {
+            name: 'SMP 03',
+            city: 'Bogor',
+            graduate: 2013
+        },
+        {
+            name: 'SMA 01',
+            city: 'Surabaya',
+            graduate: 2016
+        },
+        {
+            name: 'Universitas Maju',
+            city: 'Tangerang',
+            graduate: null
+        },
+    ],
+    favoriteRestaurant: ['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
